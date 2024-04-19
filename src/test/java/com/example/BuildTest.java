76bede5248250c83476c3ea00abe3248c816c078
Jitendra_Singh2 <Jitendra_Singh2@epam.com>
2024-04-19 17:21:03 +0530

    first commit

---
+++ src/test/java/com/example/BuildTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BuildTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
